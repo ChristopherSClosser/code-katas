a8180e07360f0afa60cd21a0224eb457685d8944
Chris Closser <chrisclosser1@gmail.com>
2017-12-28 19:11:10 -0800

    parenthetics and test

--- test_parens.py
+++ test_parens.py
@@ -11,6 +11,25 @@ def test_case_1():
     """."""
     assert vp(' (') is False
 
+
+def test_case_2():
+    """."""
+    assert vp(")test") is False
+
+
+def test_case_3():
+    """."""
+    assert vp("") is True
+
+
+def test_case_4():
+    """."""
+    assert vp("hi(hi)()") is True
+
+
+def test_case_5():
+    """."""
+    assert vp("hi())(") is False
 # Test.assert_equals(valid_parentheses("  ("),False)
 # Test.assert_equals(valid_parentheses(")test"),False)
 # Test.assert_equals(valid_parentheses(""),True)
