0f8895d28aa8cacafd89ad8e3dd356d3cf75f564
Chris Closser <chrisclosser1@gmail.com>
2018-01-10 19:14:16 -0800

    typoglycemic passing kata

--- src/typoglycemic.py
+++ src/typoglycemic.py
@@ -4,32 +4,65 @@ Messes up the inner spelling of words.
 To test this theory:
 https://en.wikipedia.org/wiki/Typoglycemia
 It's a lot harder to read than the exapmles online...
+
+---------best practice--------
+def scramble_words(words):
+    words = words.split()
+    ans = []
+    for w in words:
+        l = [[w[i],i] for i in range(len(w)) if w[i] in "-',."]
+        x = w.translate(None,"-',.")
+        x = [x[0]] + sorted(x[1:-1]) + [x[-1]] if len(x) > 1 else x
+        for c in l:
+            x.insert(c[1],c[0])
+        ans += [''.join(x)]
+    return ' '.join(ans)
 """
-# import random
 
 
 def messup(words):
-    """."""
-    result = ''
-    sub_word = ''
+    """Return string of words with the middle letters in alphabetical order."""
+    res = ''
+    sub = ''
+    word = ''
+    hold = None
+    dash = None
 
     for char in words + ' ':
-        import pdb; pdb.set_trace()
         if char.isalpha():
-            sub_word += char
+            sub += char
+        elif char == '-' or char == "'" and len(sub) > 0:
+            hold = len(sub)
+            dash = char
         else:
-            if len(sub_word) > 3:
-                result += sub_word[0]
-                result += ''.join(sorted(sub_word[1:-1], len(sub_word)-2))
-                result += sub_word[-1]
+            if len(sub) >= 3:
+                word += sub[0]
+                word += ''.join(sorted(sub[1:-1]))
+                word += sub[-1]
+                if dash:
+                    word = word[:hold] + dash + word[hold:]
+                res += word
             else:
-                result += sub_word
-            sub_word = ''
-
-            result += char
-
-    return result[:-1]
+                res += sub
+            sub = ''
+            word = ''
+            dash = None
+            hold = None
+            res += char
+    return res[:-1]
 
 
 if __name__ == '__main__':
-    print(messup("It's yanker letter"))
+    print(messup("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth."))
+
+# Test.describe("Basic tests")
+# Test.assert_equals(scramble_words('professionals'), 'paefilnoorsss', 'The first and last letters of a word should reamin in place with the inner letters sorted')
+# Test.assert_equals(scramble_words('i'), 'i', 'Must handle single charater words')
+# Test.assert_equals(scramble_words(''), '', 'Must handle empty strings')
+# Test.assert_equals(scramble_words('me'), 'me', 'Must handle 2 charater words')
+# Test.assert_equals(scramble_words('you'), 'you', 'Must handle 3 charater words')
+# Test.assert_equals(scramble_words('card-carrying'), 'caac-dinrrryg', 'Only spaces separate words and punctuation should remain at the same place as it started')
+# Test.assert_equals(scramble_words("shan't"), "sahn't", 'Punctuation should remain at the same place as it started')
+# Test.assert_equals(scramble_words('-dcba'), '-dbca', 'Must handle special character at the start')
+# Test.assert_equals(scramble_words('dcba.'), 'dbca.', 'Must handle special character at the end')
+# Test.assert_equals(scramble_words("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth."), "you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth.", 'Must handle a full sentence')
