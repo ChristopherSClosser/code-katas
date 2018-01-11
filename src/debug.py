1c157c3de9b0521db6653aa54409ad56e81c6edc
Chris Closser <chrisclosser1@gmail.com>
2018-01-11 10:16:45 -0800

    testing for debug

--- src/debug.py
+++ src/debug.py
@@ -1,26 +1,38 @@
+"""
+Take debugging to a whole new level.
+
+Given a string, remove every single bug.
+
+This means you must remove all instances of the word 'bug'
+from within a given string, unless the word is plural ('bugs').
+
+import re
+def debug(s):
+    return re.sub(re.compile('bug(?!s)'), '', s)
+"""
+
+
 def debug(text):
+    """Given a string, remove every single bug."""
     res = text[:]
-    bug_idx = [i for i in range(len(text)) if text.startswith('bug', i)]
-    bugs_idx = text.find('bugs')
-    # for 'bug' in text:
-    #     bug_idx += 1
-    # for 'bugs' in text:
-    #     bugs_idx += 1
-    if bug_idx and bugs_idx == -1:
-        import pdb; pdb.set_trace()
+    bug_idx = [
+        i for i in range(len(text)) if text.startswith('bug', i)
+    ]
+    bugs_idx = [
+        i for i in range(len(text)) if text.startswith('bugs', i)
+    ]
+    if not bug_idx:
+        return res
+    if bug_idx and not bugs_idx:
         res = res.replace('bug', '')
         return res
-    if bug_idx == -1:
-        return res
-    for char in res:
-        if char == 'b':
-            idx = res.index(char)
-            if res[idx + 1] == 'u' and res[idx + 2] == 'g':
-                if res[idx + 3] != 's':
-                    res = res.replace('bug', '', 1)
-            idx += 1
+    for idx in bugs_idx:
+        bug_idx.remove(idx)
+    if bug_idx:
+        res = res[:bug_idx[0]] + res[bug_idx[0] + 3:]
+        return debug(res)
     return res
 
 
-if __name__ == '__main__':
-    print(debug('obbugubuggo'))
+if __name__ == '__main__':  # pragma no cover
+    print(debug(''))
