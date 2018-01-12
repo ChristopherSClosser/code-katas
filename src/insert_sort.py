f24feae82b8396931c1b2fe6b6ed3903d54bd759
Chris Closser <chrisclosser1@gmail.com>
2018-01-12 10:29:58 -0800

    insert sort re factor from kata

--- src/insert_sort.py
+++ src/insert_sort.py
@@ -24,26 +24,56 @@ def insert_sort(head):
 class Node(object):
     """Node object constructor."""
 
-    def __init__(self, data):
+    def __init__(self, data, next=None):
         """."""
         self.data = data
-        self.next = None
+        self.next = next
 
 
-def insert_sort(head):
+class LinkedList(object):
+    """Class for a linked list."""
+
+    def __init__(self, iterable=None):
+        """On list creation set head to none."""
+        self.head = None
+        self.length = 0
+        if isinstance(iterable, (str, tuple, list)):
+            for i in iterable:
+                self.push(i)
+
+    def push(self, data):
+        """Push a val to the list."""
+        self.head = Node(data, self.head)
+        self.length += 1
+
+
+def insert_sort(ll):
     """Linked list insert sort, return head of list."""
-    if not head:
-        return head
-    comp, comp.next, curr = Node(0), head, head
+    if not ll:
+        return
+    count = 0
+    comp, comp.next, curr = Node(0), ll.head, ll.head
     while curr:
         if curr.next and curr.next.data < curr.data:
             pre = comp
             while pre.next and pre.next.data < curr.next.data:
                 pre = pre.next
+                count += 1
             hold = pre.next
             pre.next = curr.next
             curr.next = curr.next.next
             pre.next.next = hold
         else:
             curr = curr.next
+        count += 1
     return comp.next
+
+
+if __name__ == '__main__':  #  pragma no cover
+    ll = LinkedList((2, 88, 52, 5, 47, 1, 66, 7, 99, 4, 23, 200, 31, 300))
+    curr = insert_sort(ll)
+    res = []
+    while curr:
+        res.append(curr.data)
+        curr = curr.next
+    print(res, ll.head.data)
