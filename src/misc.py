7aa846518e3dadceba1e4aaec2cdd77c7494ef4f
Chris Closser <chrisclosser1@gmail.com>
2018-04-21 13:27:32 -0700

    odd or even dashatize

--- src/misc.py
+++ src/misc.py
@@ -49,3 +49,34 @@ ZEETALPH = {
 
 def oddOrEven(arr):
     return 'even' if sum(arr) % 2 == 0 else 'odd'
+
+
+def dashatize(num):
+    """."""
+    import re
+    return '-'.join(
+        ' '.join(re.split(r'([13579])', str(abs(num)))).split()
+    ) if num else '0' if num == 0 else 'None'
+    """
+        from random import randint
+        import re
+
+        # You can use Test.describe and Test.it to write BDD style test groupings
+        test.describe('Basic')
+        test.assert_equals(dashatize(274),"2-7-4", "Should return 2-7-4")
+        test.assert_equals(dashatize(5311),"5-3-1-1", "Should return 5-3-1-1")
+        test.assert_equals(dashatize(86320),"86-3-20", "Should return 86-3-20")
+        test.assert_equals(dashatize(974302),"9-7-4-3-02", "Should return 9-7-4-3-02")
+        test.describe('Weird')
+        test.assert_equals(dashatize(None),"None", "Should return None");
+        test.assert_equals(dashatize(0),"0", "Should return 0");
+        test.assert_equals(dashatize(-1),"1", "Should return 1");
+        test.assert_equals(dashatize(-28369),"28-3-6-9", "Should return 28-3-6-9");
+        test.describe('Random')
+        def mySolution(num):
+            p = re.compile(r"([13579])")
+            return p.sub('-\\1-',str(num)).replace('--','-').strip('-')
+        for j in range(100):
+            input = randint(0,999999999)
+            test.assert_equals(dashatize(input), mySolution(input))
+    """
